d4bd2efd424b96df223fe48ec92f7fb43d6c7058
Arshya Soleimani <arrshya@gmail.com>
2023-06-20 22:38:04 +0000

    implemented older tweet styling and hmtl with data and hover effects

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  const $textInput = $("#tweet-text");
+  
+  $textInput.on('input',function(event) {
+    event.preventDefault();
+
+    // Looks up to the parent (form) finds decendent with .counter
+    const counter =  $(this).parent().find('.counter');
+    // Sets that to 140 - the number of chars in the text area (this)
+    let currentCount = 140 - $(this).val().length;
+
+    // Checks if counter is negative and changes css color for counter
+    if (currentCount < 0) {
+      counter.css({
+        'color': 'red'
+      })
+    }
+    
+    counter.val(currentCount);
+
+
+    
+    
+    
+
+  
+    
+
+
+  });
+ 
+});
