ff15a7c1aff089a6f5ebd0e403a122d66db642f8
Arshya <arrshya@gmail.com>
2023-06-21 14:35:21 -0600

    Created ajax request to POST and GET, time changed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,67 +1,69 @@
 $(document).ready(function() {
-  renderTweets(data);
+
+  // Function to create a tweet based on data given
+  const createTweetElement = (data) => {
+    const $tweet = $(`
+    <article class="tweet">
+        <header class="tweet-header">
+          <div class="header-info">
+            <img class="tweet-image" src=${data.user.avatars}> 
+            <h3 class="tweet-name">${data.user.name}</h3>
+          </div>
+          <h4>${data.user.handle}</h4>
+        </header>
+        <div>${data.content.text}</div>
+        <div class="horizontal-line"></div>
+        <footer class="tweet-footer">
+            <h4>${timeago.format(data.created_at)}</h4>
+              <div class="footer-icons">
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-arrows-rotate"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+          </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+
+  // Function to loop through tweet data and append to tweets-container
+  const renderTweets = (tweets) => {
+    const $container = $('#tweets-container').empty();
+
+    for (const tweet of tweets) {
+      $container.prepend(createTweetElement(tweet))
+    }
+  };
+
+  // Get request to /tweets to load database of tweets
+  const loadTweets = () => {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: (tweets) => {
+        renderTweets(tweets);
+      }
+    });
+  };
+
+  loadTweets();
+
+
+  // Post request to /tweets on submission of form
+  // Tweet created will then load in dynamically
+  const $tweetForm = $('.tweet-form');
+  $tweetForm.on('submit', (event) => {
+    event.preventDefault();
+
+    const serialized = $tweetForm.serialize();
+
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:8080/tweets',
+      data: serialized,
+      success: () => {
+        loadTweets();
+      }
+    });
+  });
 });
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-// Function to create a tweet based on data given
-const createTweetElement = (data) => {
-  const $tweet = $(`
-  <article class="tweet">
-      <header class="tweet-header">
-        <div class="header-info">
-          <img class="tweet-image" src=${data.user.avatars}> 
-          <h3 class="tweet-name">${data.user.name}</h3>
-        </div>
-        <h4>${data.user.handle}</h4>
-      </header>
-      <div>${data.content.text}</div>
-      <div class="horizontal-line"></div>
-      <footer class="tweet-footer">
-          <h4>${data.created_at}</h4>
-            <div class="footer-icons">
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-arrows-rotate"></i>
-              <i class="fa-solid fa-heart"></i>
-            </div>
-        </footer>
-    </article>
-  `);
-
-  return $tweet;
-};
-
-// Function to loop through tweet data and append to tweets-container
-const renderTweets = (tweets) => {
-  const $container = $('#tweets-container').empty();
-
-  for (const tweet of tweets) {
-    $container.prepend(createTweetElement(tweet))
-  }
-};
-
-
-
